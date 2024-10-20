12097decb0a9d9b4bb70a3d557071736e432182a
s87923 <s87923@bht-berlin.de>
2024-10-20 16:11:55 +0200

    #2 main app file gefüllt, verionen für express und typescript festgelegt

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import "express-async-errors";
+
+//import {playerRouter} from './routes/player'
+
+const app = express();
+
+//Routes
+//app.use("/api/player", playerRouter)
+
+export default app;
