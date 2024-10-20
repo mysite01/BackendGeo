03582825347dec194dbb4b65178bef68ab0d39fb
s87923 <s87923@bht-berlin.de>
2024-10-20 17:17:51 +0200

    #4 created more tests, imported dependences

--- tests/model/PlayerModel.test.ts
+++ tests/model/PlayerModel.test.ts
@@ -1,6 +1,12 @@
 import { Player } from "../../src/model/PlayerModel";
 
-test("", async () => {
+test("CreatePlayer test", async () => {
     const player = await Player.create({ name: "Harry", gameId: "test"})
     expect(player).toBeTruthy();
+})
+
+test("FindPlayer test", async () => {
+    const player = await Player.create({ name: "Harry", gameId: "test"})
+    const foundPlayer = await Player.findOne({ name: "Harry"})
+    expect(foundPlayer).toBeTruthy();
 })
