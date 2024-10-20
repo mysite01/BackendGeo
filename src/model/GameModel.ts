6f742ed58d3dd12821b846235ac7298b88e5f4ed
s87923 <s87923@bht-berlin.de>
2024-10-20 17:58:59 +0200

    #3 created GameModel file and structure (not implemented yet)

--- src/model/GameModel.ts
+++ src/model/GameModel.ts
@@ -3,3 +3,11 @@ import mongoose from "mongoose";
 export interface IGame {
 
 }
+
+const gameSchema = new mongoose.Schema<IGame>(
+    {
+
+    }
+)
+
+export const Game = mongoose.model<IGame>('Game', gameSchema)
