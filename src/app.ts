816910369b6ffd1459e1631f1f5fe40c206d041e
s87923 <s87923@bht-berlin.de>
2024-10-21 17:45:03 +0200

    #3 imported Routes in main app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import "express-async-errors";
 
-//import {playerRouter} from './routes/player'
+import {playerRouter} from './routes/player'
 
 const app = express();
 
 //Routes
-//app.use("/api/player", playerRouter)
+app.use("/api/player", playerRouter)
 
 export default app;
