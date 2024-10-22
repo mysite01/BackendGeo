0041d987937d609e3f712220f94fb330007778e3
Emir K <emirkaya3610@gmail.com>
2024-10-22 02:05:15 +0200

    GameModel.ts teils fertig

--- src/model/GameModel.ts
+++ src/model/GameModel.ts
@@ -1,13 +1,29 @@
 import mongoose from "mongoose";
 
 export interface IGame {
-
+    title: string; 
+    location: {
+        type: string,  
+        coordinates: [number, number],  
+    };
+    //players: mongoose.Schema.Types.ObjectId[]; Später bearbeiten für Liste der Spieler im Game
 }
 
 const gameSchema = new mongoose.Schema<IGame>(
     {
-
+        title: { type: String, required: true },  
+        location: { //GeoJSON format für den Standort
+            type: {
+                type: String,
+                enum: ['Point'],  
+                required: true
+            },
+            coordinates: {
+                type: [Number],  
+                required: true
+            }
+        }
     }
-)
+);
 
-export const Game = mongoose.model<IGame>('Game', gameSchema)
+export const Game = mongoose.model<IGame>('Game', gameSchema);
