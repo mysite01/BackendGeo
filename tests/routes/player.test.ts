02565cc3ff606676daa874c2d75be6e9ec0e7214
s87923 <s87923@bht-berlin.de>
2024-10-27 16:40:13 +0100

    playerRoute test create player

--- tests/routes/player.test.ts
+++ tests/routes/player.test.ts
@@ -11,3 +11,22 @@ test ("Test getPlayer, korrekte eingaben", async()=>{
     expect(response.statusCode).toBe(200)
     expect(response.body.length).toBe(1)
 })
+
+test("Test createPlayer, korrekte eingaben", async()=>{
+    const testee = supertest(app)
+    const response = await testee.post(`/api/player/`).send({name: "Helke", gameId: "1"})
+    expect(response.status).toBe(201)
+    expect(response.body.name).toBe("Helke")
+})
+
+test("Test create Player, keine eingaben", async ()=> {
+    const testee = supertest(app)
+    const response = await testee.post(`/api/player/`).send()
+    expect(response.status).toBe(404)
+})
+
+test("Test create Player, falsche eingaben (falscher datentyp, gameid)", async()=>{
+    const testee = supertest(app)
+    const response = await testee.post(`/api/player/`).send({name: "Arwed", gameId: 1})
+    expect(response.status).toBe(404)
+})
