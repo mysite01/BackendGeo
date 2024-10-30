e95ffc97cf6e862230fd41544815245d77a44006
Emir K <emirkaya3610@gmail.com>
2024-10-30 18:47:05 +0100

    GameModel fertig bearbeitet

--- tests/model/GameModel.test.ts
+++ tests/model/GameModel.test.ts
@@ -1,14 +1,32 @@
 import { Game } from '../../src/model/GameModel';
 
-test("CreateGame test", async () => {
+test("CreateGame with multiple POIs", async () => {
     const game = await Game.create({
-        title: "Berlin Warschauer Straße",
-        location: {
-            type: "Point",
-            coordinates: [13.451176883277185, 52.50888599508144]
-        }
+        title: "Berlin Sehenswürdigkeiten",
+        POIs: [
+            {
+                type: "Point",
+                coordinates: [13.404954, 52.520008]  // Brandenburger Tor
+            },
+            {
+                type: "Point",
+                coordinates: [13.377704, 52.516275]  // Reichstag
+            },
+            {
+                type: "Point",
+                coordinates: [13.41053, 52.523219]   // Alexanderplatz
+            }
+        ]
     });
-    expect(game).toBeTruthy();  
-    expect(game.title).toBe("Berlin Warschauer Straße");
-    expect(game.location.coordinates).toEqual([13.451176883277185, 52.50888599508144]);
+
+    expect(game).toBeTruthy();
+    expect(game.title).toBe("Berlin Sehenswürdigkeiten");
+
+    expect(game.POIs).toBeDefined();
+    expect(game.POIs).toHaveLength(3);
+
+    expect(game.POIs[0].coordinates).toEqual([13.404954, 52.520008]);  // Brandenburger Tor
+    expect(game.POIs[1].coordinates).toEqual([13.377704, 52.516275]);  // Reichstag
+    expect(game.POIs[2].coordinates).toEqual([13.41053, 52.523219]);   // Alexanderplatz
+
 });
