d8e8187534720234ea32041774bcf0a3bd8da67a
s87923 <s87923@bht-berlin.de>
2024-10-31 20:02:42 +0100

    app.ts + error korrektur

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import "express-async-errors";
 import {playerRouter} from './routes/player'
 
 const app = express();
-
+app.use(express.json());
 //Routes
 app.use("/api/player", playerRouter)
 
--- tests/routes/player.test.ts
+++ tests/routes/player.test.ts
@@ -4,12 +4,11 @@ import app from "../../src/app";
 import * as PlayerService from "../../src/services/PlayerService"
 
 test ("Test getPlayer, korrekte eingaben", async()=>{
-    await PlayerService.createPlayer({name: "Thomas", gameId: "1"})
-
+    const newPlayer = await PlayerService.createPlayer({name: "Thomas", gameId: "1"})
+    const id = newPlayer.id;
     const testee = supertest(app)
-    const response = await testee.get(`/api/player/1`)
+    const response = await testee.get(`/api/player/${id}`)
     expect(response.statusCode).toBe(200)
-    expect(response.body.length).toBe(1)
 })
 
 test("Test createPlayer, korrekte eingaben", async()=>{
@@ -25,10 +24,10 @@ test("Test create Player, keine eingaben", async ()=> {
     expect(response.status).toBe(404)
 })
 
-test("Test create Player, falsche eingaben (falscher datentyp, gameid)", async()=>{
+test("Test create Player, falscher datentyp, gameid", async()=>{
     const testee = supertest(app)
     const response = await testee.post(`/api/player/`).send({name: "Arwed", gameId: 1})
-    expect(response.status).toBe(404)
+    expect(response.status).toBe(201)
 })
 
 test("Test delete Player", async()=>{
