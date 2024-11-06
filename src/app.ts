91906b570a3bc6750563cfaf87b9d9308abdd226
s87923 <s87923@bht-berlin.de>
2024-11-06 11:58:17 +0100

    readded cors middleware

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,21 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Benutzerdefinierte Middleware zur Konfiguration der CORS-Header
+app.use((req: Request, res: Response, next: NextFunction) => {
+    // Setze die CORS-Header
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.set("Access-Control-Expose-Headers", "Authorization");
+    res.set("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+  
+
 //Routes
 app.use("/api/player", playerRouter)
 app.use("/api/game", gameRouter);
