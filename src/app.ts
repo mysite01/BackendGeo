983011a9c46a26b786312b7ebc21bb17313d97fc
Emir K <emirkaya3610@gmail.com>
2024-11-06 18:15:18 +0100

    von main gezogen

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,30 @@ import {gameRouter} from './routes/game'
 import {gameInstanceRouter} from './routes/gameInstance'
 import { teamRouter } from './routes/team';
 
-//import cors from 'cors';
- 
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+// Benutzerdefinierte Middleware zur Konfiguration der CORS-Header
+app.use((req: Request, res: Response, next: NextFunction) => {
+    // Setze die CORS-Header
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.set("Access-Control-Expose-Headers", "Authorization");
+    res.set("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+  
+// Beispielroute
+app.get('/', (req: Request, res: Response) => {
+    res.send('CORS ist konfiguriert!');
+  });  
+
 //Routes
 app.use("/api/player", playerRouter)
 app.use("/api/game", gameRouter);
