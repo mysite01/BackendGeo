906002ff163c7bc388b057e788d9fb10a2f3c07d
s87923 <s87923@bht-berlin.de>
2024-11-06 10:04:35 +0100

    Team delete Route erstellt

--- src/routes/team.ts
+++ src/routes/team.ts
@@ -21,3 +21,28 @@ teamRouter.post("/", async (req, res, next) =>{
         next(err)
     }
 })
+
+/**
+ * Route für das entfernen von Teams
+ */
+teamRouter.delete("/:id", async (req, res, next) => {
+    let id = "";
+    if(req.params){
+        id = req.params.id
+    }
+
+    try{
+        await TeamService.deleteTeam(id)
+        res.status(204).send()
+    } catch (err){
+        res.status(404)
+        next(err)
+    }
+})
+
+/**
+ * Route fürs bekommen von einem Team
+ */
+teamRouter.get("/:id", async (req, res, next) => {
+    throw new Error("not implemented yet")
+})
