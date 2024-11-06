8122363637efc9029a351d0440d891e25208f1e6
s87923 <s87923@bht-berlin.de>
2024-11-06 11:57:05 +0100

    readded cors middleware

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,9 @@ import {gameRouter} from './routes/game'
 import { teamRouter } from './routes/team';
 
 import cors from 'cors';
- 
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 //Routes
 app.use("/api/player", playerRouter)
