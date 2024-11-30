5716a6ef7dca3389e42dbcf03a28a194731a665b
Thorge Breitzmann <s87923@bht-berlin.de>
2024-11-30 16:37:41 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ async function setup() {
     console.info(`Connecting to MongoDB at ${mongodURI}`);
     await mongoose.connect(mongodURI);
 
-    await createExampleGame;
+    await createExampleGame();
 
     const httpPort = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 3443;
     const httpServer = http.createServer(app);
