54b37f52191e0eb226544e611fed7375d2226874
s87923 <s87923@bht-berlin.de>
2024-12-10 17:29:41 +0100

    changed qr code generation

--- src/utils/Qacodegenerate.ts
+++ src/utils/Qacodegenerate.ts
@@ -5,10 +5,9 @@ export async function generateQAcode(): Promise<{ qrCodeDataUrl: string, uniqueC
     
     try {
         const uniqueCode = generateUniqueCode(); // Eindeutiger Code
-        //console.log(`${process.env.FRONTEND_URL}`)
-        const inviteUrl = `${process.env.FRONTEND_URL}/ReadQACode/${uniqueCode}`; // Einladung-URL 
+        const inviteUrl = `${process.env.CORS_ORIGIN}/ReadQACode/${uniqueCode}`; // Einladung-URL 
 
-        const shareUrl = `${process.env.FRONTEND_URL}/${uniqueCode}?feature=shared`;
+        const shareUrl = `${process.env.CORS_ORIGIN}/${uniqueCode}?feature=shared`;
         // QR-Code aus der URL generieren
         const qrCodeDataUrl = await QRCode.toDataURL(inviteUrl);
         //console.log("QR-Code erfolgreich generiert:", inviteUrl);
